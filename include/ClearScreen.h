75c91bd3d214b7ddbb5f5b6d4a3671e0a428aaaa
czg <c969407088@gmail.com>
2019-10-19 00:06:20 +0800

    Implement ClearScreen.

---
+++ include/ClearScreen.h
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iosfwd>
+
+class ClearScreen
+{
+} clear;
+
+std::ostream &operator<<(std::ostream &os, ClearScreen)
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+    return os;
+}
